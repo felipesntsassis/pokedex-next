dbc52439e1d39ea98309884f19ff77d6850294da
Felipe dos S. Assis <felipesntsassis@gmail.com>
2022-05-04 15:19:24 -0300

    Versão inicial do projeto.

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export async function getStaticProps(context) {
+    const pokemons = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+        })
+        .then(data => {
+            return data.pokemon_entries;
+        });
+    
+    return {
+        props: {
+            pokemons
+        },
+    };
+}
+
+export default function Home(props) {
+    const { pokemons } = props;
+
+    return (
+        <div>
+            Pókedex - NextJS Edition!
+            <ul>
+                {pokemons.map((pokemon) => (
+                    <li key={pokemon.entry_number}>
+                        {pokemon.pokemon_species.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+
+}
